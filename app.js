2253ff4330fff7a112a6268a133614044df3cbfb
Adam Smith <adamsmith6300@gmail.com>
2017-02-18 17:55:54 +0000

    update database url

--- app.js
+++ app.js
@@ -17,8 +17,8 @@ var commentRoutes       = require("./routes/comments"),
     indexRoutes          = require("./routes/index");
 
 
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://Adamsmith6300:Adam5561@ds023510.mlab.com:23510/yelpcamp");
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://Adamsmith6300:Adam5561@ds023510.mlab.com:23510/yelpcamp");
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
